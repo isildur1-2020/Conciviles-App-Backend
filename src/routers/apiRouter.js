96b46a53b4ea428ff3a51ccaa5de50d0194e5b89
Esteban G <tapitarias@gmail.com>
2022-05-05 17:24:47 -0500

    add print logic

--- src/routers/apiRouter.js
+++ src/routers/apiRouter.js
@@ -3,7 +3,9 @@ export const apiRouter = Router();
 import { mainController } from "../controllers/mainController";
 import { infoController } from "../controllers/infoController";
 import { employeeController } from "../controllers/employeeController";
+import { print } from "../print/index";
 
 apiRouter.post("/", mainController);
 apiRouter.get("/info", infoController);
 apiRouter.get("/employees", employeeController);
+apiRouter.get("/file", print);
--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -3,8 +3,6 @@ export const rootRouter = Router();
 import { apiRouter } from "./apiRouter";
 import { authMiddleware } from "../middleware/jwt";
 import { loginController } from "../controllers/loginController";
-import { print } from "../print/index";
 
 rootRouter.use("/api", authMiddleware, apiRouter);
 rootRouter.post("/login", loginController);
-rootRouter.get("/file", print);
