42ea8b7ef050ce53cb4aebec20f64ec1341e73ad
Esteban G <tapitarias@gmail.com>
2022-05-21 19:19:19 -0500

    add roles

--- src/controllers/loginController/isSupervisor.js
+++ src/controllers/loginController/isSupervisor.js
@@ -3,10 +3,10 @@ export const isSupervisor = (req, res, next) => {
   req.charge = Number(req.user?.info?.["cargo6"]?.value?.ids?.[0]);
   //17 8 25 13
   if (
-    req.charge !== 16 ||
-    req.charge !== 17 ||
-    req.charge !== 8 ||
-    req.charge !== 25 ||
+    req.charge !== 16 &&
+    req.charge !== 17 &&
+    req.charge !== 8 &&
+    req.charge !== 25 &&
     req.charge !== 13
   )
     return res.status(200).json({
