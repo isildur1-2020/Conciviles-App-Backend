b1d1172491181b5150d2c40353217bda28362a9e
Esteban G <tapitarias@gmail.com>
2022-05-21 19:14:18 -0500

    add roles

--- src/controllers/loginController/isSupervisor.js
+++ src/controllers/loginController/isSupervisor.js
@@ -1,7 +1,14 @@
 export const isSupervisor = (req, res, next) => {
   // VERIFICA SI ES SUPERVISOR
   req.charge = Number(req.user?.info?.["cargo6"]?.value?.ids?.[0]);
-  if (req.charge !== 16)
+  //17 8 25 13
+  if (
+    req.charge !== 16 ||
+    req.charge !== 17 ||
+    req.charge !== 8 ||
+    req.charge !== 25 ||
+    req.charge !== 13
+  )
     return res.status(200).json({
       message: "Este usuario no tiene permisos",
       err: true,
